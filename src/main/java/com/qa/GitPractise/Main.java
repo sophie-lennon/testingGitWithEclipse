7b779af60e39a6267b8e14d725bc6b654b438c0d
Sophie Lennon <sophielennon95@gmail.com>
2017-09-19 15:52:53 +0000

    extra output to main

--- src/main/java/com/qa/GitPractise/Main.java
+++ src/main/java/com/qa/GitPractise/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Holy cow we did it");
+		System.out.println("pls still work");
 
 	}
 
