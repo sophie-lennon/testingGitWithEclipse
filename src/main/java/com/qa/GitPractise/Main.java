6ef2d49d829f54cedef4b2537fb5a787816aa57b
Sophie Lennon <sophielennon95@gmail.com>
2017-09-19 16:11:56 +0000

    trying to cause a merge conflict

--- src/main/java/com/qa/GitPractise/Main.java
+++ src/main/java/com/qa/GitPractise/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Holy cow we did it");
+		System.out.println("pls forgive me merge");
 		System.out.println("pls still work");
 		
 		String message = "hello github";
