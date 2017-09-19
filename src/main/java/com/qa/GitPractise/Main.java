29f88b2c7f7f5b0d5c7cb095941364aa2511400f
James Leek <jameswilliamleek@gmail.com>
2017-09-19 16:12:57 +0000

    Think we avoided the merge conflict

--- src/main/java/com/qa/GitPractise/Main.java
+++ src/main/java/com/qa/GitPractise/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("lol merge");
 		System.out.println("Holy cow we did it");
 		System.out.println("pls still work");
 		
